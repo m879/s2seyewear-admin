aeffb11d9f6ef87d283513b0a50d1b48cc51e8d0
Meraj <merajahmed2112@gmail.com>
2022-02-14 13:05:53 +0530

    url changed

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -21,33 +21,7 @@ export const loadUser = () => (dispatch) => {
   
 // REGISTER USER
 export const userlogin = (userData) => (dispatch) => {
-    console.log("Actions = " , userData)
-    // const {username,password}=userData;
-
-    // const token = `${username}:${password}`;
-    // const encodedToken = Buffer.from(token).toString('base64');
-    // const session_url = `${baseURL}/api/login`;
-
-    // var config = {
-    //   method: 'post',
-    //   url: session_url,
-    //   headers: { 'Authorization': 'Basic '+ encodedToken }
-    // };
-
-    // axios(config).then(function (response) {
-    //   console.log(JSON.stringify(response.data));
-    //   dispatch({
-    //     type: LOGIN_SUCCESS,
-    //     payload: response.data,
-    //   });
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    //   dispatch({
-    //       type: LOGIN_FAIL,
-    //   });
-    // });
-
+  
     const config = {
       headers: {
         'Content-Type': 'application/json',
@@ -68,29 +42,6 @@ export const userlogin = (userData) => (dispatch) => {
         });
       });
 };
-
-// REGISTER USER
-// export const register = (userData) => (dispatch) => {
-//     console.log("Actions = " , userData)
-//     const config = {
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     };
-//     axios.post(`${baseURL}/api/auth/register`,userData,config)
-//     .then((res) => {
-//       console.log('Register seccessfully');
-//       dispatch({
-//         type: REGISTER_SUCCESS,
-//         payload: res.data,
-//       });
-//     })
-//     .catch((err) => {
-//         dispatch({
-//           type: REGISTER_FAIL,
-//         });
-//       });
-// };
 
 // LOGOUT USER
 export const logout = () => (dispatch) => {
--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const baseURL='https://s2seye.herokuapp.com'
+// export const baseURL='https://s2seye.herokuapp.com'
+export const baseURL='https://api.s2seyewear.com'
